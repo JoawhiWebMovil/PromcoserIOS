10d6633a2aa0d7aca70d4488fc89300a5824bcf6
Joaquin Delgado Lorino <jdelglor@emeal.nttdata.com>
2024-11-21 19:42:19 -0500

    Initial commit

---
+++ PromcoserIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PromcoserIOS
+//
+//  Created by Joaquin Delgado Lorino on 21/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
